b1556a193ae53007cfc0236ef2dc7512812e42f8
tibetdev <tibetdev@gmail.com>
2024-03-19 23:14:17 +0100

    loginpage layout is about to done

---
+++ src/components/MessageBubble.tsx
@@ -0,0 +1,12 @@
+import '../styles/MessageBubble.css';
+function MessageBubble() {
+  return (
+    <div className='talk-bubble round '>
+      <div className='talktext'>
+        <p>May I have your user name?</p>
+      </div>
+    </div>
+  );
+}
+
+export default MessageBubble;
